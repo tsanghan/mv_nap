09e87075a8c3cc990939c7895e37cbb3cba2afde
tsanghan <tsanghan@gmail.com>
2022-08-25 15:11:13 +0800

    changed conn number from 100 to 145

--- upstream.js
+++ upstream.js
@@ -3,7 +3,7 @@ async function resolv(r) {
         let reply = await ngx.fetch('http://127.0.0.1:8090/api/7/http/requests');
         let body = await reply.json();
 
-        if (parseInt(body.current) > 100) {
+        if (parseInt(body.current) > 145) {
             r.headersOut['upstream'] = "httpbin_server/delay/3";
         } else {
             r.headersOut['upstream'] = "juice_server";
